963b34a9e7278d45dd19525d3fc394491e4c1ce9
Araraki <misewang@gmail.com>
2019-05-11 00:18:35 +0800

    add Post-processing shaders

---
+++ Assets/7.Post-processing/EdgeDetectNormalsAndDepth.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class EdgeDetectNormalsAndDepth : PostProcessingBase
+{
+	public Shader edgeDetectShader;
+	private Material edgeDetectMaterial;
+	public Material material
+	{
+		get
+		{
+			edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
+			return edgeDetectMaterial;
+		}
+	}
+
+	[Range(0.0f, 1.0f)]
+	public float edgeOnly = 0.0f;
+	public Color edgeColor = Color.black;
+	public Color backgroundColor = Color.white;
+	public float sampleDistance = 1.0f;
+	public float sensitivityDepth = 1.0f;
+	public float sensitivityNormals = 1.0f;
+
+	void OnEnable()
+	{
+		GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+	}
+
+	[ImageEffectOpaque]
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		if (material != null)
+		{
+			material.SetFloat("_EdgeOnly", edgeOnly);
+			material.SetColor("_EdgeColor", edgeColor);
+			material.SetColor("_BackgroundColor", backgroundColor);
+			material.SetFloat("_SampleDistance", sampleDistance);
+			material.SetVector("_Sensitivity", new Vector4(sensitivityNormals, sensitivityDepth, 0.0f, 0.0f));
+
+			Graphics.Blit(src, dest, material);
+		}
+		else
+		{
+			Graphics.Blit(src, dest);
+		}
+	}
+}
